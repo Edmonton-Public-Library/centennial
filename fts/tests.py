f25959442d57ef3880aaf36f6a768597cf1d4ce3
Oscar Ramirez <oscar@rmz.ca>
2013-01-11 22:45:53 -0700

    Compacted selenium feature tests
    Selenium testing is real slow, since these are feature
    tests I guess I can live with them being bigger and less
    modular for sake of speeding up testing.

--- fts/tests.py
+++ fts/tests.py
@@ -12,20 +12,20 @@ class AdminTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_admin_page_reachable(self):
+    def test_admin_page_login(self):
+        self.admin_page_reachable()
+        self.admin_login()
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+    def admin_page_reachable(self):
         self.browser.get(self.live_server_url + '/admin/')
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
 
-    def test_admin_page_login(self):
-        self.browser.get(self.live_server_url + '/admin/')
+    def admin_login(self):
         username_field = self.browser.find_element_by_name('username')
         username_field.send_keys('admin')
-
         password_field = self.browser.find_element_by_name('password')
         password_field.send_keys('asdf')
         password_field.send_keys(Keys.RETURN)
-
-        # Validate in Site Administration page
-        body = self.browser.find_element_by_tag_name('body')
-        self.assertIn('Site administration', body.text)
