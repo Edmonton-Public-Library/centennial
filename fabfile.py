3ab9b5907e2f8425cda9687ca200340ba74cd0b9
Oscar Ramirez <oscar@rmz.ca>
2013-01-19 09:52:36 -0700

    Removed path dependency
    Cleaned up to avoid keeping the db file under git

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,18 @@
 from fabric.api import local, lcd
 
-def prepare_deployment(branch_name):
-    local('python manage.py test epl')
-    local('git add -p && git commit')
-    local('git checkout master && git merge ' + branch_name)
+def test():
+    local('python manage.py test timemap')
+    local('python manage.py test fts')
 
-def deploy():
-    with lcd('~/tmp/epl/'):
-        local('git pull ~/epl')
+#def prepare_deployment(branch_name):
+#    local('python manage.py test epl')
+#    local('git add -p && git commit')
+#    local('git checkout master && git merge ' + branch_name)
+#
+#def deploy():
+#    with lcd('~/tmp/epl/'):
+#        local('git pull ~/epl')
 #        local('python manage.py migrate epl')
 #        local('python manage.py test epl')
-        #local(restart webserver)
+#        local(restart webserver)
 
