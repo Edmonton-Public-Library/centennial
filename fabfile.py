ca09fc797b957f34accac5aa236db608e7ba1439
Oscar Ramirez <oscar@rmz.ca>
2013-01-29 12:35:25 -0700

    Added updatedb fab command

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,14 @@
 from fabric.api import local, lcd
+from fabric.context_managers import settings
 
 def test():
     local('python manage.py test timemap')
     local('python manage.py test fts')
+
+def updatedb():
+    with settings(warn_only=True):
+        local('python manage.py migrate fts')
+        local('python manage.py migrate timemap')
 
 #def prepare_deployment(branch_name):
 #    local('python manage.py test epl')
