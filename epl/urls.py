ccd3879bd9ca8db3e9e4b7fe6b53cf52c825509b
Oscar Ramirez <oscar@rmz.ca>
2013-01-31 11:47:20 -0700

    Storie Model is now exposed through tastypie
    Full REST access to the story model is currently setup. Once users
    are created we will need to change this to use the sessions.

--- epl/urls.py
+++ epl/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
 
-from timemap.api import BranchResource
+from timemap.api import BranchResource, StoryResource
 from timemap.views import timemap
 
 admin.autodiscover()
 v1_api = Api(api_name="v1")
 v1_api.register(BranchResource())
+v1_api.register(StoryResource())
 
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- timemap/tests.py
+++ timemap/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from timemap.models import Branch
 from timemap.models import Story
 from django.core.exceptions import ValidationError
+from util.tastytest import ResourceTestCase
 
 class BranchModelTest(TestCase):
 
@@ -57,3 +58,45 @@ def save_branch(name, description, start_year, end_year, longitude, latitude):
     branch.latitude = latitude
     branch.save()
     return branch
+
+class EntryResourceTest(ResourceTestCase):
+    fixtures = ['branches.json']
+
+    def setUp(self):
+        super(EntryResourceTest, self).setUp()
+
+        self.capilano_branch = Branch.objects.get(name="Capilano")
+        self.capilano_url = '/api/v1/branch/{0}/'.format(self.capilano_branch.pk)
+
+    def get_credentials(self):
+        pass
+
+    def test_get_branch_json(self):
+        resp = self.api_client.get(self.capilano_url, format='json')
+        self.assertValidJSONResponse(resp)
+        keys = ['description',
+                'end_year',
+                'id',
+                'latitude',
+                'longitude',
+                'name',
+                'resource_uri',
+                'start_year']
+        self.assertKeys(self.deserialize(resp), keys)
+        self.assertEqual(self.deserialize(resp)['name'], "Capilano")
+
+    def test_add_story(self):
+        story = {"branch": self.capilano_url,
+                 "day": 30,
+                 "description": "For children up to age three",
+                 "link_url": "http://www.shareedmonton.ca/",
+                 "month": 1,
+                 "public_approved": True,
+                 "resource_uri": "/api/v1/story/1/",
+                 "story_text": "Location\tInformation\tRegistration",
+                 "title": "Sing, Sign, Laugh and Learn",
+                 "year": 2013
+                }
+
+        resp = self.api_client.post('/api/v1/story/', data=story)
+        self.assertHttpCreated(resp)
