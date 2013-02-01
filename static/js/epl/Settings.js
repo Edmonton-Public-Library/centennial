6fb8abac4c7892282c8b4ec592a1a39f745bb09b
--local <timdphillips@gmail.com>
2013-02-01 14:32:02 -0700

    Fixing routes to uploadStory and example views.

--- static/js/epl/Settings.js
+++ static/js/epl/Settings.js
@@ -25,13 +25,15 @@ return {
 			//The template within /static/views/example.html will be loaded into the viewport provided to the AppClass constructor
 			//@see the csc.View constructor for information on transitioning between views using the parameters below
 			require(['epl', 'views'], function (epl, views) {
-				epl.nav.transition(epl.views.example);
+				epl.nav.transition(views.example);
 			});
 		});
 		
 		//Route to Upload Story view
 		this.get('#uploadStory', function () {
-			epl.nav.transition(epl.views.uploadStory);
+			require(['epl', 'views'], function (epl, views) {
+				epl.nav.transition(views.uploadStory);
+			});
 		});
 
 		self.get('', function () {
