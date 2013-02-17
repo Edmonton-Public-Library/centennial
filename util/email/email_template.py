53a2a514500aa19a8fbdcaa5701774d6983ff76d
Noorez Kassam <noorez@ualberta.ca>
2013-02-17 15:28:18 -0700

    add some initial email templates

---
+++ util/email/email_template.py
@@ -0,0 +1,3 @@
+REGISTRATION_NOTIFICATION = """Thank you {%username} for registering with EPL. To activate your account, you will need to verify your account by navigating to the following URL: {%registration_url}"""
+
+PASSWORD_RESET_EMAIL = """To reset your password, please click on the following link: {%forgot_password_url}"""
