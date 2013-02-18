7a0065f3f882ac209be029dae5d0a5dbf694f06b
Oscar Ramirez <oscar@rmz.ca>
2013-02-18 11:02:05 -0700

    Fix tests
    Oops, removed sites app, but forgot the fixtures included data dependant on that.
    This updates the fixtures to remove entries dependant on the sites app

--- epl/settings.py
+++ epl/settings.py
@@ -116,8 +116,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.sites',
-    #'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
     'fts',
