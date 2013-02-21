f529555280e80d3c4cbf9824a73e550adfac0987
Taylor Lloyd <taylor@taylorlloyd.ca>
2013-02-20 20:14:07 -0700

    Unit Tests for user content

--- util/bibliotest.py
+++ util/bibliotest.py
@@ -31,9 +31,36 @@ class BiblicommonsTest(unittest.TestCase):
             mockGet.return_value = result
             self.assertFalse(bibliocommons.validUser("user", "pass"))
 
+    def testUserContentNone(self):
+        result = Mock()
+        result.json.return_value = { 'page':'1', 'pages':'1', 'user_content':[]}
+        #Patch requests.get
+        with patch.object(requests, 'get') as mockGet:
+            mockGet.return_value = result
+            self.assertTrue(bibliocommons.userContent('user') == [])
 
+    def testUserContentOnePage(self):
+        result = Mock()
+        result.json.return_value = { 'page':'1', 'pages':'1', 'user_content':['one', 'two', 'three']}
+        #Patch requests.get
+        with patch.object(requests, 'get') as mockGet:
+            mockGet.return_value = result
+            self.assertTrue(bibliocommons.userContent('user') == ['one', 'two', 'three'])
 
+    def testUserContentManyPage(self):
+        #Patch requests.get
+        with patch.object(requests, 'get') as mockGet:
+            mockGet.side_effect = self.manypage_sideeffect
+            self.assertTrue(bibliocommons.userContent('user') == ['1', '2', '3', '4'])
 
+    def manypage_sideeffect(*args, **kwargs):
+        params = kwargs['params']
+        page = 1
+        if ('page' in params):
+            page = params['page']
+        result = Mock()
+        result.json.return_value = { 'page':str(page), 'pages':'4', 'user_content':[str(page)]}
+        return result
 
 if __name__ == '__main__':
     unittest.main()
