d86c3e3452cf645ae2ef84242bdf0b2d84db8f4e
Braeden Petruk <braedenpetruk@gmail.com>
2013-03-08 12:21:22 -0700

    Added missing Authentication.js

---
+++ static/js/epl/Authentication.js
@@ -0,0 +1,25 @@
+;
+define(['epl/Environment', 'lib/knockout'], function (Environment, ko) {
+
+return (function () {
+
+	var Authentication = function () { };
+
+	Authentication.logIn = function (credentials, success, error) {
+		$.ajax(Environment.apiBase + '/account/login/centennial', {
+			data : credentials,
+			dataType : 'json',
+			success : success,
+			error: error
+		});
+	};
+
+	ko.bindingHandlers.authenticateSubmit = function (form, fieldIDs) {
+		//Write code!
+	};
+
+	return Authentication;
+
+})();
+
+});
