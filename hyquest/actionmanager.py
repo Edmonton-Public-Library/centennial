695504285f4a7d14f7e3de7b0255c54a76ce6d0c
Taylor Lloyd <taylor@taylorlloyd.ca>
2013-03-14 16:26:10 -0600

    Fixed actionmanager methods

--- hyquest/actionmanager.py
+++ hyquest/actionmanager.py
@@ -6,11 +6,15 @@ from datetime import datetime
 
 def beginQuestSet(user, questset):
     #Create the UserQuestSetAction Object
+    uqsa=UserQuestSetAction.objects.create(user=user, questset=questset, beginTime=datetime.now())
+    uqsa.save()
     #Delegate for all Quests in QuestSet
+    for quest in Quest.objects.filter(questset=questset):
+        beginQuest(user, quest)
 
 def beginQuest(user, quest):
     #Create the UserQuestAction Object
-    uqa=UserQuestAction.create(user=user, quest=quest, begintime=datetime.now())
+    uqa=UserQuestAction.objects.create(user=user, quest=quest, beginTime=datetime.now())
     uqa.save()
     #Delegate for all Tasks in Quest
     for task in Task.objects.filter(quest=quest):
@@ -18,18 +22,51 @@ def beginQuest(user, quest):
 
 def beginTask(user, task):
     #Create the UserTaskAction Object
-    uta=UserTaskAction.create(user=user, task=task, begintime=datetime.now())
+    uta=UserTaskAction.objects.create(user=user, task=task, beginTime=datetime.now())
     uta.save()
 
 def completeQuestSet(user, questset):
-    #Check that all quests for this set are complete
-    #If so, Load and complete the UserQuestSetAction
+    #Check that all tasks for this questset are complete
+    complete=True
+    for uqa in UserQuestAction.objects.filter(quest__questset=questset):
+        if not uqa.complete:
+            complete=False
+    
+    if complete:
+        #If so, Load and Complete the UserQuestSetAction
+        try:
+            uqa=UserQuestSetAction.get(user=user, questset=questset)
+            uqa.complete=True
+            uqa.completionTime=datetime.now()
+            uqa.save()
+        except ObjectDoesNotExist:
+            pass
 
 def completeQuest(user, quest):
     #Check that all tasks for this quest are complete
-    #If so, Load and Complete the UserQuestAction
-    #and Delegate upwards to the QuestSet 
+    complete=True
+    for uta in UserTaskAction.objects.filter(task__quest=quest):
+        if not uta.complete:
+            complete=False
+
+    if complete:
+        #If so, Load and Complete the UserQuestAction
+        try:
+            uqa=UserQuestAction.get(user=user, quest=quest)
+            uqa.complete=True
+            uqa.completionTime=datetime.now()
+            uqa.save()
+            completeQuestSet(user=user, questset=uqa.quest.questset)
+        except ObjectDoesNotExist:
+            pass
 
 def completeTask(user, task):
     #Load and Complete the UserTaskAction
-    #Delegate upwards to the quest
+    try:
+        uta=UserTaskAction.get(user=user, task=task)
+        uta.complete=True
+        uta.completionTime=datetime.now()
+        uta.save()
+        completeQuest(user=user, quest=uta.task.quest)
+    except ObjectDoesNotExist:
+        pass
