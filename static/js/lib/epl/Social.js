d737714c6bcfdebab0fdc30a3bdd451a26758650
Noorez Kassam <noorez@ualberta.ca>
2013-03-16 16:20:12 -0600

    add social javascript module

---
+++ static/js/lib/epl/Social.js
@@ -0,0 +1,28 @@
+;
+define(function() {
+
+var Social = function() {};
+
+Social.postToFeed = function(shareLink, shareImage, shareName, shareCaption, shareDescription) {
+
+        // calling the API ...
+        var obj = {
+          method: 'feed',
+          link: shareLink,
+          picture: shareImage,
+          name: shareName,
+          caption: shareCaption,
+          description: shareDescription,
+        }
+
+        function callback(response) {
+            if(response && !response.post_id) {
+                alert("error posting to facebook feed");
+            }
+        }
+
+        FB.ui(obj, callback);
+     };
+
+     return Social;
+});
