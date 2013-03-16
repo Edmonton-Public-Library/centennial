70ab02999344474138fc8394c6158c95588e4393
Taylor Lloyd <taylor@taylorlloyd.ca>
2013-03-16 09:49:59 -0600

    unbreak the build

--- epl/urls.py
+++ epl/urls.py
@@ -8,7 +8,7 @@ from tastypie.api import Api
 from timemap.api import BranchResource, StoryResource, UserResource, SimpleBranchResource
 from timemap.views import timemap, upload
 from centennial.views import accountActivate, login_user, logout_user, create_user, current_user, link_bibliocommons, update_user
-from hyquest.api import FeaturedQuestSetResource, ActiveQuestSetResource, QuestResource, TaskResource
+from hyquest.api import QuestSetResource, QuestResource, TaskResource
 
 admin.autodiscover()
 v1_api = Api(api_name="v1")
@@ -16,8 +16,7 @@ v1_api.register(BranchResource())
 v1_api.register(SimpleBranchResource())
 v1_api.register(StoryResource())
 v1_api.register(UserResource())
-v1_api.register(FeaturedQuestSetResource())
-v1_api.register(ActiveQuestSetResource())
+v1_api.register(QuestSetResource())
 v1_api.register(QuestResource())
 v1_api.register(TaskResource())
 
