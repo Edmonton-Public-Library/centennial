fa902e0ffcaf5affb523bac7996c501f407b4f62
Braeden Petruk <braedenpetruk@gmail.com>
2013-03-16 18:36:17 -0600

    Started working on search results scrolling.

--- static/js/timemap/Sidebar.js
+++ static/js/timemap/Sidebar.js
@@ -89,6 +89,10 @@ define(['lib/knockout', 'lib/csc/Utils', 'timemap/Environment', 'lib/epl/Input']
 
 			this.data = {
 				searchResults : ko.observable([]),
+				searchHeight : ko.computed = function () {
+					//-80 for search stuff on top
+					return Environment.display.height() - Environment.display.topBarHeight() - 80;
+				},
 				resultClick : function (data) {
 					document.location = '#viewStory/' + data.id;
 				},
