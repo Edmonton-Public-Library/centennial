0c681078ca2547f389e17a2520fec014d0124186
Noorez Kassam <noorez@ualberta.ca>
2013-03-23 14:31:55 -0600

    test adding social media share buttons

--- static/js/timemap.js
+++ static/js/timemap.js
@@ -37,7 +37,7 @@ var AppClass = (function () {
 
 			Environment.sidebar.setFeaturedStoriesSource('all');
 
-			self.initFacebook();
+			//self.initFacebook();
 
 			ko.applyBindings({
 				Environment: Environment
@@ -48,31 +48,6 @@ var AppClass = (function () {
 		//Bootstrap components that depend on the DOM
 		this.nav = new Nav(this.viewport, Routes.routes);
 
-	};
-
-	AppClass.prototype.initFacebook = function () {
-		window.fbAsyncInit = function() {
-			// init the FB JS SDK
-			FB.init({
-				appId      : Settings.apiKeys.facebook.connect, // App ID from the App Dashboard
-				//channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File for x-domain communication
-				status     : true, // check the login status upon init?
-				cookie     : true, // set sessions cookies to allow your server to access the session?
-				xfbml      : true  // parse XFBML tags on this page?
-			});
-		};
-
-		// Load the SDK's source Asynchronously
-		// Note that the debug version is being actively developed and might 
-		// contain some type checks that are overly strict. 
-		// Please report such bugs using the bugs tool.
-		(function(d, debug){
-		   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-		   if (d.getElementById(id)) {return;}
-		   js = d.createElement('script'); js.id = id; js.async = true;
-		   js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
-		   ref.parentNode.insertBefore(js, ref);
-		 }(document, /*debug*/ false));
 	};
 
 	return AppClass;
