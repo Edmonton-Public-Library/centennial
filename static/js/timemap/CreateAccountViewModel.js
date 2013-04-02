703fcd7ad0dd4e278619f50083ff435701572dd7
Tim Phillips <timdphillips@gmail.com>
2013-04-02 14:21:15 -0600

    Success message on account creation.

--- static/js/timemap/CreateAccountViewModel.js
+++ static/js/timemap/CreateAccountViewModel.js
@@ -47,7 +47,8 @@ return (function () {
                 dataType: "json",
                 contentType: "application/json",
                 success: function () {
-                    // top.location="#createAccountSuccess";
+                    top.location.reload(true);
+                    top.location="#createAccountSuccess";
                 }, 
                 error: function (xhr) {
                      $('#createAccountSubmit').attr('disabled', false);
