061b54d8e36fdafdbe4dc3879d0242b54588e6a4
Rstuvi <Isaac.Matichuk@gmail.com>
2013-08-21 15:14:52 -0600

    test

--- hyquest/verifiers/bibliocommons.py
+++ hyquest/verifiers/bibliocommons.py
@@ -54,7 +54,7 @@ def bibliocommonsMatches(contentSet, task):
         if 'format' in reqs and reqs['format'] != content['title']['format']['id']:
             continue
         if 'title' in reqs and reqs['title'] != content['title']['title']:
-            raise Exception(reqs['title'])
+            raise Exception(content['title']['title'])
             continue
         if 'author' in reqs:
             reqAuthors = reqs['author'].split(':')
