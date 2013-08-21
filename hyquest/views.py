8a17ceb6338fe839b9a50593cf0ca3cf115ed3f3
Rstuvi <Isaac.Matichuk@gmail.com>
2013-08-21 14:21:04 -0600

    test

--- hyquest/views.py
+++ hyquest/views.py
@@ -28,7 +28,7 @@ def hyq(request):
 
 
 def check_biblio_tasks(request):
-    return HttpResponse(json.dumps({'Response' : request.user}), status=404, content_type='application/json');
+    return HttpResponse(json.dumps({'Response' : request.user.username}), status=404, content_type='application/json');
     if not request.user.is_authenticated():
         return HttpResponse(json.dumps({'Response':'Error: Must be logged in'}), status=403, content_type='application/json')
     if not verifyBibliocommonsAccount(request.user):
